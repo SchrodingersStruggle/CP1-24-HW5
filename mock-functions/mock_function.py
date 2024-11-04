2def182c1a66506fa939c7b0b221118b0cbee58f
SchrodingersStruggle <coreyyou@buffalo.edu>
2024-11-04 13:06:05 -0500

    Create mock_function.py
    Creates a mock function to later test.

---
+++ mock-functions/mock_function.py
@@ -0,0 +1,14 @@
+"""
+Tests Repository setup with mock function.
+Goal is to see how linting and testing goes.
+"""
+def mock_function(message = 'message')
+  """
+  Prints whatever message you put in the function.
+  This is just a placeholder to test the setup.
+  Parameters:
+  - Message (str): Input message to print.
+  Returns:
+  - Prints message as output.
+  """
+  return print(message)
